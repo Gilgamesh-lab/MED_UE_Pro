10e2cf744ef735c2626fd66ff3d8556adf3b51be
orchid127 <ndougakimberley@gmail.com>
2026-05-25 17:17:00 +0200

    implémentation serveur http + bouton rechercher

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
+
 function Sidebar() {
+    const [departure, setDeparture] = useState("");
+    const [arrival, setArrival] = useState("")
+    const [route, setRoute] = useState("")
+
+    const handleSearch = async () => {
+        const response = await fetch(`http://localhost:8080/chemin?departure=${departure}&arrival=${arrival}`);
+        const routeText = await response.text();
+        setRoute(routeText);
+    }
+
     return (
         <div className="flex flex-col m-5">
             <div className="mb-4">
                 <h1 className="text-2xl text-[#123ABF] font-extrabold">itinéraire</h1>
             </div>
-            <div className="flex flex-col w-full">
+            <div className="flex flex-col w-full mb-4">
                 <p className="text-lg text-[#123ABF] mb-2">départ</p>
-                <input type="" placeholder="ex: Maison Blanche" className="border border-[#c0caf8] h-10 w-full p-2 mb-3 rounded-[10px] text-lg text-[#123ABF] focus:outline-0" />
+                <input type="text" value={departure} onChange={(e) => setDeparture(e.target.value)} placeholder="ex: Maison Blanche" className="border border-[#c0caf8] h-10 w-full p-2 mb-3 rounded-[10px] text-lg text-[#123ABF] focus:outline-0" />
 
                 <p className="text-lg text-[#123ABF] mb-2">arrivée</p>
-                <input type="" placeholder="ex: Villejuif Louis Aragon" className="border border-[#c0caf8] h-10 w-full p-2 mb-3 rounded-[10px] text-lg text-[#123ABF] focus:outline-0" />
+                <input type="text" value={arrival} onChange={(e) => setArrival(e.target.value)} placeholder="ex: Villejuif Louis Aragon" className="border border-[#c0caf8] h-10 w-full p-2 mb-3 rounded-[10px] text-lg text-[#123ABF] focus:outline-0" />
 
-                <button className="border border-[#c0caf8] hover:border-[#c0caf8] focus:border-[#123ABF] bg-white hover:bg-[#c0caf8]/20 focus:bg-[#123ABF] text-lg text-[#123ABF] focus:text-white rounded-[10px] p-[0.25rem]">rechercher</button>
+                <button onClick={handleSearch} className="border border-[#c0caf8] hover:border-[#c0caf8] focus:border-[#123ABF] bg-white hover:bg-[#c0caf8]/20 focus:bg-[#123ABF] text-lg text-[#123ABF] focus:text-white rounded-[10px] p-[0.25rem]">rechercher</button>
+            </div>
+            <div>
+                {route && <p className="text-[#123ABF]">{route}</p>}
             </div>
         </div>
     )
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,64 +6,116 @@ import org.example.graphe.Resultat;
 import org.example.graphe.Station;
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
-
+import com.sun.net.httpserver.*;
 
 public class Main {
-    static void main() throws FileNotFoundException {
-        // 159 ; Lamarck Caulaincourt ;12 ;False; 0
-        // 0000; Abbesses ;12 ;False; 0
-        //0;159;46
-        Station Abbesses = new Station("Abbesses", 0, false, 0, "12");
-        Station LamarckCaulaincourt = new Station("Lamarck Caulaincourt", 159, false, 0, "12");
+    static void main() throws FileNotFoundException, IOException {
+        /*
+         * 
+         * // 159 ; Lamarck Caulaincourt ;12 ;False; 0
+         * // 0000; Abbesses ;12 ;False; 0
+         * // 0;159;46
+         * Station Abbesses = new Station("Abbesses", 0, false, 0, "12");
+         * Station LamarckCaulaincourt = new Station("Lamarck Caulaincourt", 159, false,
+         * 0, "12");
+         * 
+         * System.out.println(Abbesses.getVoisins());
+         * System.out.println(LamarckCaulaincourt.getVoisins());
+         * 
+         * Arete arete = new Arete(Abbesses, LamarckCaulaincourt, 46, "12");
+         * 
+         * System.out.println(Abbesses.getVoisins());
+         * System.out.println(LamarckCaulaincourt.getVoisins());
+         * 
+         * Graphe graphe = new Graphe();
+         * System.out.println(graphe.findStation(16).getNom());
+         * System.out.println(graphe.findStation("Bastille").getLignes());
+         * System.out.println(graphe.findStation("Bastille").getAretes());
+         * Resultat resultat = graphe.getBFS("Bastille");
+         * System.out.println(resultat.getChemin());
+         * 
+         * System.out.println("");
+         * if (graphe.getStations().stream().allMatch(station -> station.isMarquer())) {
+         * System.out.println("Le graphe est connexe");
+         * } else {
+         * System.out.println("Le graphe n'est pas connexe");
+         * }
+         * 
+         * if (graphe.getStations().stream().allMatch(station -> station.getX() != 0 &&
+         * station.getY() != 0)) {
+         * System.out.println("Toutes les stations ont des coordonnées");
+         * } else {
+         * System.out.println("Des coordonnées pour certaines stations sont manquante");
+         * graphe.getStations().stream()
+         * .filter(station -> station.getX() == 0 || station.getY() == 0)
+         * .forEach(station -> System.out.println(station.getNom()));
+         * }
+         * System.out.println("");
+         * System.out.println("");
+         * ArrayList<Arete> aretes = graphe.getKruskal();
+         * aretes.stream().forEach(
+         * arete2 -> System.out.println(arete2.getSommet1().getNom() + " -> " +
+         * arete2.getSommet2().getNom()
+         * + " (métro " + arete2.getLigne() + ") " + arete2.getTempsEnSecondes() +
+         * " secondes"));
+         * System.out.println();
+         * System.out.println("Poids total du plus court chemin trouvé = "
+         * + (aretes.stream().mapToInt(arete2 -> arete2.getTempsEnSecondes()).sum()) /
+         * 360 + " heures");
+         * 
+         * System.out.println();
+         * System.out.
+         * println("Algorithme de Dijkstra entre Maison Blanche et Villejuif Louis Aragon"
+         * );
+         * System.out.println();
+         * 
+         * System.out.println();
+         * resultat = graphe.getDijkstra("Maison Blanche", "Villejuif, Louis Aragon");
+         * 
+         * System.out.println(
+         * "Le chemin le plus court trouvé entre Maison Blanche et Villejuif, Louis Aragon avec l’algorithme de Dijkstra est : "
+         * + resultat.getChemin() + " avec un poids minimun de " + resultat.getPoids());
+         */
 
-        System.out.println(Abbesses.getVoisins());
-        System.out.println(LamarckCaulaincourt.getVoisins());
+        System.out.println("------------------------");
 
-        Arete arete = new Arete(Abbesses, LamarckCaulaincourt, 46, "12");
+        // creation serveur http
+        HttpServer serveur = HttpServer.create(new InetSocketAddress(8080), 0);
 
-        System.out.println(Abbesses.getVoisins());
-        System.out.println(LamarckCaulaincourt.getVoisins());
+        serveur.createContext("/chemin", exchange -> {
+            // evite de bloquer les requêtes du front end
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
 
-        Graphe graphe = new Graphe();
-        System.out.println(graphe.findStation(16).getNom());
-        System.out.println(graphe.findStation("Bastille").getLignes());
-        System.out.println(graphe.findStation("Bastille").getAretes());
-        Resultat resultat = graphe.getBFS("Bastille");
-        System.out.println(resultat.getChemin());
+            // récupère de la requête en front end
+            String query = exchange.getRequestURI().getQuery();
+            String depart = "";
+            String arrivee = "";
 
-        System.out.println("");
-        if (graphe.getStations().stream().allMatch(station -> station.isMarquer())) {
-            System.out.println("Le graphe est connexe");
-        } else {
-            System.out.println("Le graphe n'est pas connexe");
-        }
+            // récupère les valeurs des stations de départ et d'arrivée
+            for (String param : query.split("&")) {
+                String[] pair = param.split("=");
+                if (pair[0].equals("departure"))
+                    depart = pair[1];
+                if (pair[0].equals("arrival"))
+                    arrivee = pair[1];
+            }
 
-        if (graphe.getStations().stream().allMatch(station -> station.getX() != 0 && station.getY() != 0)) {
-            System.out.println("Toutes les stations ont des coordonnées");
-        } else {
-            System.out.println("Des coordonnées pour certaines stations sont manquante");
-            graphe.getStations().stream()
-                    .filter(station -> station.getX() == 0 || station.getY() == 0)
-                    .forEach(station -> System.out.println(station.getNom()));
-        }
-        System.out.println("");
-        System.out.println("");
-        ArrayList<Arete> aretes = graphe.getKruskal();
-        aretes.stream().forEach(arete2 -> System.out.println(arete2.getSommet1().getNom() + " -> " + arete2.getSommet2().getNom() + " (métro " + arete2.getLigne() + ") " + arete2.getTempsEnSecondes() + " secondes"));
-        System.out.println();
-        System.out.println("Poids total du plus court chemin trouvé = " + (aretes.stream().mapToInt(arete2 -> arete2.getTempsEnSecondes()).sum()) / 360 + " heures");
+            // calcule le plus court chemin
+            Graphe newGraphe = new Graphe();
+            String chemin = newGraphe.getDijkstra(depart, arrivee).getChemin();
 
+            // renvoie le résultat
+            byte[] response = chemin.getBytes();
+            exchange.sendResponseHeaders(200, response.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response);
+            os.close();
+        });
 
-        System.out.println();
-        System.out.println("Algorithme de Dijkstra entre Maison Blanche et Villejuif Louis Aragon");
-        System.out.println();
+        serveur.start();
+        System.out.println("the server is running on port 8080 :D");
 
-        System.out.println();
-        resultat = graphe.getDijkstra("Maison Blanche", "Villejuif, Louis Aragon");
-
-            System.out.println("Le chemin le plus court trouvé entre Maison Blanche et Villejuif, Louis Aragon avec l’algorithme de Dijkstra est : " + resultat.getChemin() + " avec un poids minimun de " + resultat.getPoids());
-
-
-        }
     }
+}
